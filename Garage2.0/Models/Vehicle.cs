8a5181b2df6dd81eb5c655eeca2f801c2ae7eeb4
Jerry Fan <jerry_swe@hotmail.com>
2018-05-30 14:58:59 +0200

    Create Vehicle-class in Model-folder

---
+++ Garage2.0/Models/Vehicle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage2._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public virtual int TypeID
+        {
+            get
+            {
+                return (int)this.VehicleType;
+            }
+            set
+            {
+                VehicleType = (VehicleTypes)value;
+            }
+        }
+
+        [EnumDataType(typeof(VehicleTypes))]
+        [Display(Name = "Vehicle Type")]
+        public VehicleTypes VehicleType { get; set; }
+
+        public enum VehicleTypes
+        {
+            Motocycle,
+            Car,
+            Truck,
+            Van
+        }
+
+        [Required]
+        [StringLength(6, ErrorMessage = "No more than 6 letters are allowed here!")]
+        public string RegNum { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+
+        [Required]
+        public DateTime ParkTime { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Range: 0 - 100")]
+        public int NumOfTires { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "No more than 30 letters are allowed here!")]
+        public string Model { get; set; }
+    }
+}
